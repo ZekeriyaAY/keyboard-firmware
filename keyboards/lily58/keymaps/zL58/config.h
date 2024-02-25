2e008fe31e70932c51340b0c8585505b00efcaba
Zekeriya AY <zekeriya@zekeriyaay.com>
2024-02-25 17:11:09 +0300

    zL58 files added

---
+++ keyboards/lily58/keymaps/zL58/config.h
@@ -0,0 +1,16 @@
+// Copyright 2024 Zekeriya AY (@ZekeriyaAY)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/* Select hand configuration */
+
+#define MASTER_LEFT
+// #define MASTER_RIGHT
+// #define EE_HANDS
+
+#define QUICK_TAP_TERM 0
+#define TAPPING_TERM 200
+
+/*  HARDWARE OPTIONS */
+#define USE_I2C
---
+++ keyboards/lily58/keymaps/zL58/rules.mk
@@ -0,0 +1,23 @@
+LTO_ENABLE = yes            # Link Time Optimization enabled
+BOOTMAGIC_ENABLE = yes       # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes        # Mouse keys
+EXTRAKEY_ENABLE = yes        # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = no            # N-Key Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+AUDIO_ENABLE = no           # Audio output
+RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
+SWAP_HANDS_ENABLE = no      # Enable one-hand typing
+OLED_ENABLE = no           # OLED display
+
+SPLIT_KEYBOARD = yes
+
+# If you want to change the display of OLED, you need to change here
+# SRC +=  ./lib/rgb_state_reader.c \
+#         ./lib/layer_state_reader.c \
+#         ./lib/logo_reader.c \
+#         ./lib/keylogger.c \
+        # ./lib/mode_icon_reader.c \
+        # ./lib/host_led_state_reader.c \
+        # ./lib/timelogger.c \
